9ac9bfa8bbafcfdce33214b360191e650d03f671
가현 <52996905+kimgostring@users.noreply.github.com>
2023-02-13 20:51:31 +0900

    [Silver III] Title: 1로 만들기, Time: 568 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,25 +1,15 @@
-from collections import deque
-
 n = int(input())
 
+# dp
+arr = [0] * (n + 1) 
+arr[1] = 0
 
-def bfs(n):
-    cnt = 0
-    q = deque([(n, cnt)])
+for i in range(2, n + 1):
+    arr[i] = arr[i - 1] + 1
 
-    while q:
-        n, cnt = q.popleft()
+    if i % 3 == 0:
+        arr[i] = min(arr[i], arr[i // 3] + 1)
+    if i % 2 == 0:
+        arr[i] = min(arr[i], arr[i // 2] + 1)
 
-        if n == 1:
-            break
-
-        if n % 3 == 0:
-            q.append((n // 3, cnt + 1))
-        if n % 2 == 0:
-            q.append((n // 2, cnt + 1))
-        q.append((n - 1, cnt + 1))
-
-    return cnt
-
-
-print(bfs(n))
+print(arr[n])

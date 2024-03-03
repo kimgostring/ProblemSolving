136bc3a2bb10c2b2b2e6746d2293708f09b74d95
가현 <52996905+kimgostring@users.noreply.github.com>
2024-03-03 20:30:23 +0900

    [level 3] Title: 순위, Time: 187.11 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.py
+++ 프로그래머스/3/49191. 순위/순위.py
@@ -1,11 +1,8 @@
-from collections import deque
-
 def solution(n, results):
     graph = [[0] * (n + 1) for _ in range(n + 1)];
     for result in results:
         winner, loser = result;
         graph[winner][loser] = 1;
-        
         
     for k in range(1, n + 1):
         for i in range(1, n + 1):

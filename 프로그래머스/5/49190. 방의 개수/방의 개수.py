850b057728ccc3252ba70f778795d5fe38c8b73e
가현 <52996905+kimgostring@users.noreply.github.com>
2024-03-04 21:21:28 +0900

    [level 5] Title: 방의 개수, Time: 391.67 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/5/49190. 방의 개수/방의 개수.py
+++ 프로그래머스/5/49190. 방의 개수/방의 개수.py
@@ -1,50 +1,25 @@
+import math
+
 def solution(arrows):
     DIRS = [(0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1)];
     
+    v = set();
+    e = set();
+
     answer = 0;
     x, y = 0, 0;
-    board = dict();
-    for d in arrows:
-        # x <-> x <-> x 와 같은 식으로 2배수해서 edge를 이을 것
-        # 이때, 첫/끝 지점에는 edge 하나 
-        # 중간 지점에는 edge 두 개가 이어지게 됨
-        # 이미 있는 edge가 아닌 edge가 해당 점에 생기는 case = 방 하나 생김   
-        revD = (d + 4) % 8;
+    v.add((x, y));
+    for d in arrows:  
         dx, dy = DIRS[d];
         
-        if (x, y) in board and d in board[(x, y)]:
-            # 이미 방문한 길인 경우, 고려 X
-            x += dx * 2;
-            y += dy * 2;
-            continue;
-        
         for i in range(2):
-            # i번째 - i+1번째 점 연결
-            tempX = x + (dx * (i + 1));
-            tempY = y + dy * (i + 1);
-            addLineToBoard(tempX - dx, tempY - dy, d, board);
-            addLineToBoard(tempX, tempY, revD, board);
-        
-        notRoomDirs = [[d, revD], [revD]];
-        for i in range(2):
-            # i번째 점에서 다른 방향으로 그었던 선이 있는지 check
-            tempX = x + dx * (i + 1);
-            tempY = y + dy * (i + 1);
-            for j in board[(tempX, tempY)]: 
-                if j not in notRoomDirs[i]:
-                    answer += 1;
-                    break;
-        
-        x += dx * 2;
-        y += dy * 2;
-    
-    return answer;
+            x2, y2 = x + dx, y + dy;
+            v.add((x2, y2));
+            e.add(((x, y), (x2, y2)) if d < 4 else ((x2, y2), (x, y)));
+            x, y = x2, y2;
+            
+    return 1 - len(v) + len(e); # v - e + f = 1
 
-def addLineToBoard(x, y, d, board):
-    if (x, y) not in board:
-        board[(x, y)] = [d];
-    else:
-        board[(x, y)].append(d);
             
             
     

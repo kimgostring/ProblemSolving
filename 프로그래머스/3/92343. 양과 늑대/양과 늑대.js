6d6903986cdf0d8cfc6c224838cc2c65701eb308
가현 <52996905+kimgostring@users.noreply.github.com>
2024-06-14 17:56:08 +0900

    [level 3] Title: 양과 늑대, Time: 0.66 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.js
@@ -0,0 +1,24 @@
+function solution(info, edges) {
+    const graph = Array.from(Array(info.length), () => []);
+    edges.forEach((edge) => {
+        const [parent, child] = edge;
+        graph[parent].push(child);
+    });
+    
+    return dfs(0, [], [0, 0], info, graph);
+}
+
+function dfs(now, nexts, cnt, info, graph) {
+    cnt[info[now]]++;
+    if (cnt[1] >= cnt[0]) return;
+    
+    let answer = cnt[0];
+    nexts = [...nexts, ...graph[now]];
+    nexts.forEach((next, ind) => {
+        const temp = dfs(next, nexts.slice(ind + 1), [...cnt], info, graph);
+        if (temp > answer) answer = temp;
+        nexts.push(next);
+    });
+    
+    return answer;
+}

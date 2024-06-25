981b17f9a9c973d6d60fd96268f5eab12f9ecf32
가현 <52996905+kimgostring@users.noreply.github.com>
2024-06-25 21:49:13 +0900

    [Gold II] Title: 2048 (Easy), Time: 172 ms, Memory: 31820 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.py
@@ -1,10 +1,4 @@
 import copy;
-from enum import Enum;
-
-LEFT = 0;
-RIGIT = 1;
-UP = 2;
-DOWN = 3;
 
 n = int(input());
 arr = [];
@@ -13,57 +7,49 @@ for i in range(n):
     arr.append(list(map(int, input().split())));
     biggest = max(biggest, *arr[i]);
 
+def rotate(arr, d):    
+    new = [[0] * n for _ in range(n)];
+    if d == 0:
+        for i in range(n):
+            for j in range(n):
+                new[i][j] = arr[i][j];
+    elif d == 1:
+        for i in range(n):
+            for j in range(n):
+                new[i][j] = arr[j][n - 1 - i];
+    elif d == 2:
+        for i in range(n):
+            for j in range(n):
+                new[i][j] = arr[n - 1 - i][n - 1 - j];
+    elif d == 3:
+        for i in range(n):
+            for j in range(n):
+                new[i][j] = arr[n - 1 - j][i];
+    
+    return new;
+        
 def move(arr, d):
     global biggest;
-    new = copy.deepcopy(arr);
+    new = rotate(arr, d);
     
-    # x, y 뒤집기
-    if d == UP or d == DOWN: 
-        new = list(map(list, zip(*new)));
-    
-    # 1. 한 방향으로 옮기기
     for i in range(n):
         new[i] = [x for x in new[i] if x != 0];
-
-    # 2. 앞/뒤에서부터 합치기 & 부족한 길이만큼 뒤/앞에 0 추가
-    if d == LEFT or d == UP:
-        # 앞에서부터 합치고, 뒤쪽에 0 추가
-        for i in range(n):
-            l = len(new[i]);
-            j = 0;
-            tmp = [];
-            while j <= l - 1:
-                if j < l - 1 and new[i][j] == new[i][j + 1]:
-                    tmp.append(new[i][j] << 1);
-                    if tmp[-1] > biggest:
-                        biggest = tmp[-1];
-                    j += 2;
-                else:
-                    tmp.append(new[i][j]);
-                    j += 1;
-                
-            new[i] = tmp + [0] * (n - len(tmp));
-    else:
-        # 뒤에서부터 합치고, 앞쪽에 0 추가
-        for i in range(n):
-            l = len(new[i]);
-            j = l - 1;
-            tmp = [];
-            while j >= 0:
-                if j > 0 and new[i][j] == new[i][j - 1]:
-                    tmp.append(new[i][j] << 1);
-                    if tmp[-1] > biggest:
-                        biggest = tmp[-1];
-                    j -= 2;
-                else:
-                    tmp.append(new[i][j]);
-                    j -= 1;
-                
-            new[i] = [0] * (n - len(tmp)) + tmp[::-1];
-    
-    # x, y 뒤집기
-    if d == UP or d == DOWN: 
-        new = list(map(list, zip(*new)));
+        
+    for i in range(n):
+        l = len(new[i]);
+        j = 0;
+        tmp = [];
+        while j <= l - 1:
+            if j < l - 1 and new[i][j] == new[i][j + 1]:
+                tmp.append(new[i][j] << 1);
+                if tmp[-1] > biggest:
+                    biggest = tmp[-1];
+                j += 2;
+            else:
+                tmp.append(new[i][j]);
+                j += 1;
+            
+        new[i] = tmp + [0] * (n - len(tmp));
     
     return new;
 
@@ -72,10 +58,8 @@ def backtracking(arr, moveCnt):
         return;
     
     for d in range(4):
-        new = move(arr, d);
-        # print(moveCnt);
-        # print(*new, sep="\n");
-        backtracking(new, moveCnt + 1);
+        tmp = move(arr, d);
+        backtracking(tmp, moveCnt + 1);
     
 backtracking(arr, 0);
 print(biggest);

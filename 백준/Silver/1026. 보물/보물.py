a62fde339185863ec4fd5eb0815396e47f1ed906
가현 <52996905+kimgostring@users.noreply.github.com>
2024-07-03 16:59:46 +0900

    [Silver IV] Title: 보물, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,8 @@
+import sys;
+input = sys.stdin.readline;
+
+n = int(input());
+a = sorted(list(map(int, input().split(" "))));
+b = sorted(list(map(int, input().split(" "))), reverse=True);
+
+print(sum(a[i] * b[i] for i in range(n)));

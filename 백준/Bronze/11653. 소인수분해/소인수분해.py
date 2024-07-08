9e2ebf184ebf8cc9f529286633c00da9641d092c
가현 <52996905+kimgostring@users.noreply.github.com>
2024-07-08 19:06:23 +0900

    [Bronze I] Title: 소인수분해, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,16 +1,12 @@
-import math;
-
 n = int(input());
 answer = [];
 
 i = 2;
 while i * i <= n:
     while n % i == 0:
-        answer.append(i);
+        print(i);
         n //= i;
     i += 1;
 
 if n != 1:
-    answer.append(n);
-
-print(*answer, sep="\n");
+    print(n);

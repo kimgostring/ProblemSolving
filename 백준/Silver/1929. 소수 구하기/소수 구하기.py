8d99dbbaf8d8ab8df0adb74e6ed71ade0e63ead5
가현 <52996905+kimgostring@users.noreply.github.com>
2024-07-08 18:48:05 +0900

    [Silver III] Title: 소수 구하기, Time: 292 ms, Memory: 42796 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -8,7 +8,7 @@ for i in range(2, n + 1):
     
     if i >= m:
         answer.append(i);
-    for j in range(i * 2, n + 1, i):
+    for j in range(i * i, n + 1, i):
         arr[j] = False;
     
 print(*answer, sep="\n");

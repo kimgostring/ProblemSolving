616c00ca44eb239b0712e26486ed4019bea82e14
가현 <52996905+kimgostring@users.noreply.github.com>
2024-07-10 15:34:50 +0900

    [Silver I] Title: 카잉 달력, Time: 1008 ms, Memory: 11568 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+
+const path = process.platform === "linux" ? 0 : "input.txt";
+const [t, ...ls] = fs.readFileSync(path, encoding="utf8").trim().split("\n")
+    .map((e, i) => {
+        if (i === 0) return Number(e);
+        else return e.split(" ").map(Number);
+    });
+
+const gcd = (a, b) => {
+    const r = a % b;
+    if (!r) return b 
+    return gcd(b, r);
+};
+const lcm = (a, b) => a / gcd(a, b) * b;
+
+let answers = "";
+for (const l of ls) {
+    let [m, n, x, y] = l;
+    if (m < n) [m, n, x, y] = [n, m, y, x];
+
+    if (m === x && n === y) {
+        answers += `${lcm(m, n)}\n`;
+        continue;
+    } 
+
+    const remain = x === m ? 0 : x;
+    let answer = -1;
+    for (let i = 0; i < lcm(m, n); i += n) {
+        const year = i + y;
+        if (year % m === remain) {
+            answer = year;
+            break;
+        }
+    }
+    answers += `${answer}\n`;
+}
+
+console.log(answers);

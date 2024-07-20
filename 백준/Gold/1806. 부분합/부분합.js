f8112456f04c207a94085ce919cfbd670259d0ae
가현 <52996905+kimgostring@users.noreply.github.com>
2024-07-20 18:12:42 +0900

    [Gold IV] Title: 부분합, Time: 208 ms, Memory: 17008 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.js
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -3,19 +3,16 @@ const path = process.platform === "linux" ? 0 : "input.txt";
 
 const [[n, s], arr] = fs.readFileSync(path, encoding="utf8").trim().split("\n")
     .map(val => val.split(" ").map(Number));
-const prefixSum = arr.reduce((acc, cur, ind) => {
-        acc.push(cur + acc[ind]);
-        return acc;
-    }, [0]);
 
 let answer = Infinity;
-let [start, end] = [0, 1];
+let sum = arr[0];
+let [start, end] = [0, 0];
 while (start <= end) {
-    while (end < prefixSum.length && prefixSum[end] - prefixSum[start] < s) end++;
-    if (end >= prefixSum.length) break;
+    while (end < n && sum < s) sum += arr[++end];
+    if (end >= n) break;
     
-    answer = Math.min(answer, end - start);
-    start++;
+    answer = Math.min(answer, end - start + 1);
+    sum -= arr[start++];
 }
 
 console.log(Number.isFinite(answer) ? answer : 0);

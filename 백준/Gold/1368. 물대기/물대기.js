a8dc5c606ba7fb1724eefd0185220572e89e4732
가현 <52996905+kimgostring@users.noreply.github.com>
2024-08-12 15:51:17 +0900

    [Gold II] Title: 물대기, Time: 284 ms, Memory: 33692 KB -BaekjoonHub

--- 백준/Gold/1368. 물대기/물대기.js
+++ 백준/Gold/1368. 물대기/물대기.js
@@ -3,28 +3,20 @@ const path = process.platform === "linux" ? 0 : "input.txt";
 
 const inputs = fs.readFileSync(path, "utf8").trim().split("\n");
 const n = Number(inputs[0]);
-const nodes = [];
 const edges = [];
 for (let i = 0; i < n; i++) {
-    nodes.push([Number(inputs[i + 1]), i]);
+    edges.push([Number(inputs[i + 1]), i, n]);
 
     const prices = inputs[i + n + 1].split(" ").map(Number);
     for (let j = 0; j < n; j++) {
         if (i !== j) edges.push([prices[j], i, j]);
     }
 }
-nodes.sort((a, b) => a[0] - b[0]);
 edges.sort((a, b) => a[0] - b[0]);
-
-// 다 순회했음을 표시 
-nodes.push([Infinity]);
-edges.push([Infinity]);
 
 class UnionFind {
     constructor(n) {
-        this.parent = Array.from(Array(n), (v, i) => i)
-        this.watered = Array(n).fill(false); 
-        this.cnt = Array(n).fill(1);
+        this.parent = Array.from(Array(n), (v, i) => i);
     }
 
     find(a) {
@@ -40,55 +32,27 @@ class UnionFind {
     }
 
     union(a, b) {
-        if (this.isInSameSet(a, b)) return 0;
-        if (this.isWatered(a) && this.isWatered(b)) return 0;
+        if (this.isInSameSet(a, b)) return;
 
         let [pa, pb] = [this.find(a), this.find(b)];
         if (pa > pb) [pa, a, pb, b] = [pb, b, pa, a];
 
-        let cnt = 0;
-        if (this.isWatered(pa)) cnt = this.cnt[pb];
-        else if (this.isWatered(pb)) cnt = this.water(pa);
-
         this.parent[pb] = pa;
         this.find(b);
-        this.cnt[pa] += this.cnt[pb]; 
-        return cnt;
-    }
-
-    isWatered(a) {
-        return this.watered[this.find(a)];
-    }
-
-    water(a) {
-        if (this.isWatered(a)) return 0;
-
-        const pa = this.find(a);
-        this.watered[pa] = true;
-        return this.cnt[pa];
+        return;
     }
 }
 
+let cnt = 0;
+let res = 0;
 const uf = new UnionFind(n);
-const [nl, el] = [nodes.length - 1, edges.length - 1];
-let [ni, ei, cnt, res] = [0, 0, 0, 0];
-while (cnt < n) {
-    while (ni < nl && uf.isWatered(nodes[ni][1])) ni++;
-    while (
-        ei < el && (uf.isInSameSet(edges[ei][1], edges[ei][2]) || 
-        (uf.isWatered(edges[ei][1]) && uf.isWatered(edges[ei][2]))
-    )) ei++;
-        
-    const [node, edge] = [nodes[ni], edges[ei]];
-    if (node[0] <= edge[0]) {
-        cnt += uf.water(node[1]);
-        res += node[0];
-        ni++;
-    } else {
-        cnt += uf.union(edge[1], edge[2]);
-        res += edge[0];
-        ei++;
-    }
+for (const e of edges) {
+    const [p, a, b] = e;
+    if (uf.isInSameSet(a, b)) continue;
+    
+    uf.union(a, b);
+    res += p;
+    if (++cnt === n) break;
 }
 
 console.log(res);

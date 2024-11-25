9c6184d522711f4539c2fa516a1cd89f6f56bb7f
가현 <52996905+kimgostring@users.noreply.github.com>
2024-11-25 15:56:09 +0900

    [Gold III] Title: 소수의 연속합, Time: 780 ms, Memory: 155544 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,32 @@
+n = int(input())
+
+
+# 1. 에라토스테네스의 체 -> n보다 작거나 같은 소수 구하기
+isPrime = [True] * (n + 1)
+for p in range(2, n + 1):
+    m = 2
+    while p * m <= n:
+        isPrime[p * m] = False
+        m += 1
+
+primes = list(map(lambda x : x[0], filter(lambda x : x[1], enumerate(isPrime))))[2:]
+LEN = len(primes)
+
+# 2. 투포인터
+ans = 0
+if n >= 2:
+    s, e = 0, 1
+    now = primes[s]
+    while True:
+        if now < n:
+            if e >= LEN:
+                break
+            now += primes[e]
+            e += 1
+        else:
+            if now == n:
+                ans += 1
+            now -= primes[s]
+            s += 1
+        
+print(ans)

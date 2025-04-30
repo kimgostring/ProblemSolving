f8d297ce1e0e3cb674374686067f7ee261832198
가현 <52996905+kimgostring@users.noreply.github.com>
2025-04-30 17:21:08 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 2110.49 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.py
@@ -0,0 +1,28 @@
+def solution(diffs, times, limit):
+    l = len(diffs)
+    def calc_tot_time(level):
+        tot_time = 0
+        
+        for i in range(l):
+            if diffs[i] <= level:
+                tot_time += times[i]
+            else:
+                tot_time += (times[i - 1] + times[i]) * (diffs[i] - level) + times[i]
+                
+        return tot_time
+    
+    # parametric_search
+    ans = -1
+    start = 1
+    end = 100001 # 포함 X
+    while start < end:
+        mid = (start + end) // 2
+        tot_time = calc_tot_time(mid)
+        
+        if tot_time <= limit:
+            ans = mid
+            end = mid
+        else: # 해결 불가 
+            start = mid + 1
+            
+    return ans

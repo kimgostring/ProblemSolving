ddc3085059ceed98e9c77b3e5c7404a9b4516119
가현 <52996905+kimgostring@users.noreply.github.com>
2025-07-08 16:37:32 +0900

    [Gold III] Title: 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 - 1, Time: 2144 ms, Memory: 169712 KB -BaekjoonHub

--- 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.py
+++ 백준/Gold/20440. 🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1/🎵니가 싫어 싫어 너무 싫어 싫어 오지 마 내게 찝쩍대지마🎵 － 1.py
@@ -1,36 +1,37 @@
 import sys
 input = sys.stdin.readline
-from heapq import heapify, heappush, heappop
+from heapq import heappush, heappop
 
 END = 2100000010
 
 n = int(input())
-mosquitoes_not_in_yet = [tuple(map(int, input().rstrip().split(" "))) for _ in range(n)]
-heapify(mosquitoes_not_in_yet)
+mosquitoes = [tuple(map(int, input().rstrip().split(" "))) for _ in range(n)]
+mosquitoes.sort()
 
-now = 0
-ans_num = 0 # 마지막에 -1 해주기  
+m_idx = 0
+now_t = 0
+ans_num = 0 # 마지막에 -1 해주기
 ans_term = [-1, -1]
 mosquitoes_in = [END]
-while now != END:
-    while mosquitoes_not_in_yet and mosquitoes_not_in_yet[0][0] < mosquitoes_in[0]:
-        now, e = heappop(mosquitoes_not_in_yet)
-        heappush(mosquitoes_in, e)
-        if len(mosquitoes_in) == ans_num and now == ans_term[1]:
+while now_t != END:
+    while m_idx < n and mosquitoes[m_idx][0] < mosquitoes_in[0]:
+        now_t, end_t = mosquitoes[m_idx]
+        m_idx += 1
+        
+        heappush(mosquitoes_in, end_t)
+        if len(mosquitoes_in) == ans_num and now_t == ans_term[1]:
             # now 시간에 한 모기가 나가고 다른 모기가 바로 들어온 case
             edit_end_term_flag = True
         elif len(mosquitoes_in) > ans_num:
             edit_end_term_flag = True
             ans_num = len(mosquitoes_in)
-            ans_term[0] = now
+            ans_term[0] = now_t
 
-    while mosquitoes_in and (
-        not mosquitoes_not_in_yet or mosquitoes_in[0] <= mosquitoes_not_in_yet[0][0]
-    ):
-        now = heappop(mosquitoes_in)
+    while mosquitoes_in and (m_idx >= n or mosquitoes_in[0] <= mosquitoes[m_idx][0]):
+        now_t = heappop(mosquitoes_in)
         if ans_num == len(mosquitoes_in) + 1 and edit_end_term_flag:
             edit_end_term_flag = False
-            ans_term[1] = now
+            ans_term[1] = now_t
 
 print(ans_num - 1)
 print(*ans_term, sep=" ")

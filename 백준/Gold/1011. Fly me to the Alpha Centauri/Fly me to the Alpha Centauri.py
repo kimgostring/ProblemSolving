da8a8deffa4237e846b6a83774253edda0992800
가현 <52996905+kimgostring@users.noreply.github.com>
2025-08-13 18:57:32 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.py
@@ -14,10 +14,10 @@ for _ in range(t):
         print(1)
         continue
     
-    # n(n + 1) < y - x <= (n + 1)^2일 경우, 이동횟수 2n + 1번
-    # n^2 < y - x <= n(n + 1)일 경우, 이동횟수 2n번
-    n = math.ceil(math.sqrt(y - x)) - 1
-    if n ** 2 + n < y - x:
+    # n^2 < 루트 (y - x) <= (n + 1)^2 일 경우, 이동횟수 2n or 2n + 1번 
+    dist = y - x
+    n = math.ceil(math.sqrt(dist)) - 1
+    if n ** 2 + n < dist:
         print(2 * n + 1)
     else:
         print(2 * n)
